ab4ca1ddc3bc68b7191cd602b7fa5cf8902a24ed
Alan Pina <alanipa021@gmail.com>
2026-05-12 18:13:20 -0600

    version1

---
+++ view/pReporte2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace view
+{
+    public partial class pReporte2 : Form
+    {
+        public pReporte2()
+        {
+            InitializeComponent();
+        }
+    }
+}
